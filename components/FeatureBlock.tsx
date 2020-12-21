4bc34238555b262ef5a3d891d48895a7737bdfd3
drklee3 <derrick@dlee.dev>
2020-12-20 21:30:36 -0800

    feat: Add shadow for feature blocks

--- components/FeatureBlock.tsx
+++ components/FeatureBlock.tsx
@@ -75,7 +75,7 @@ export default function FeatureBlock(props: Props) {
 
     return (
         <div className="sm:max-w-1/2 mb-4 p-2">
-            <div className="h-full p-4 px-5 border rounded-lg border-gray-800 bg-gray-900 bg-opacity-50">
+            <div className="h-full p-4 px-5 border shadow rounded-lg border-gray-800 bg-gray-900 bg-opacity-50">
                 <Icon
                     type={icon}
                     className="w-10 mb-2 stroke-2 stroke-current"
