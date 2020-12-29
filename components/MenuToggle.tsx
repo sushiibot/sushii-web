d369c742d13063829d37deb16c9065224f4c21a8
drklee3 <derrick@dlee.dev>
2020-12-28 21:07:46 -0800

    feat: Add responsive navbar and basic animation

---
+++ components/MenuToggle.tsx
@@ -0,0 +1,42 @@
+import { motion, SVGMotionProps } from "framer-motion";
+
+const Path = (props: SVGMotionProps<SVGPathElement>) => (
+    <motion.path
+        fill="transparent"
+        strokeWidth="2"
+        stroke="currentColor"
+        strokeLinecap="round"
+        {...props}
+    />
+);
+
+interface MenuToggleProps {
+    toggle: () => void;
+}
+
+export const MenuToggle = ({ toggle }: MenuToggleProps) => (
+    <button onClick={toggle} className="sm:hidden">
+        <svg className="h-8 w-8 inline-block" viewBox="0 0 23 23">
+            <Path
+                variants={{
+                    closed: { d: "M 2 2.5 L 20 2.5" },
+                    open: { d: "M 3 16.5 L 17 2.5" },
+                }}
+            />
+            <Path
+                d="M 2 9.423 L 20 9.423"
+                variants={{
+                    closed: { opacity: 1 },
+                    open: { opacity: 0 },
+                }}
+                transition={{ duration: 0.1 }}
+            />
+            <Path
+                variants={{
+                    closed: { d: "M 2 16.346 L 20 16.346" },
+                    open: { d: "M 3 2.5 L 17 16.346" },
+                }}
+            />
+        </svg>
+    </button>
+);
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { MenuToggle } from "./MenuToggle";
+import { motion } from "framer-motion";
+
+const linksVariants = {
+    open: {
+        transition: { staggerChildren: 0.05, delayChildren: 0 },
+    },
+    closed: {
+        transition: { staggerChildren: 0.05, staggerDirection: -1 },
+    },
+};
+
+const linkVariants = {
+    open: {
+        y: 0,
+        opacity: 1,
+    },
+    closed: {
+        y: 20,
+        opacity: 0,
+    },
+};
 
 const links = [
-    { href: "/", label: "Home" },
     { href: "/about", label: "About" },
     { href: "/commands", label: "Commands" },
     { href: "/leaderboard", label: "Leaderboard" },
@@ -11,24 +33,63 @@ const links = [
 
 export default function Nav() {
     const { pathname } = useRouter();
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        setVisible(!visible);
+    };
 
     return (
         <div className="border-b border-gray-800">
             <header className="max-w-screen-lg mx-auto px-4">
-                <nav className="py-6 transition-colors text-md font-medium lowercase tracking-widest">
-                    {links.map(({ href, label }) => (
-                        <Link key={`${href}${label}`} href={href}>
-                            <a
-                                className={
-                                    "dib text-gray-200 hover:text-blue-400 mr-4" +
-                                    (pathname == href ? " text-blue-400" : "")
-                                }
-                            >
-                                {label}
-                            </a>
+                <motion.nav
+                    initial={false}
+                    className="py-6 sm:flex sm:items-center"
+                    animate={visible ? "open" : "closed"}
+                >
+                    <div className="flex items-center justify-between">
+                        <Link href="/">
+                            <img
+                                className="h-8 w-8 inline-block"
+                                src="/images/sushii2.png"
+                            />
                         </Link>
-                    ))}
-                </nav>
+                        <MenuToggle toggle={() => toggleVisible()} />
+                    </div>
+                    <motion.ul
+                        className={
+                            "flex flex-wrap sm:items-center flex-col sm:flex-row \
+                            transition-colors text-md font-medium \
+                            lowercase tracking-widest" +
+                            (visible ? " mt-4 sm:mt-0" : "")
+                        }
+                        variants={linksVariants}
+                    >
+                        {links.map(({ href, label }, i) => (
+                            <motion.li
+                                key={`${href}${label}`}
+                                variants={linkVariants}
+                            >
+                                <Link href={href}>
+                                    <a
+                                        className={
+                                            "text-gray-200 hover:text-blue-400 mx-2 sm:mx-4" +
+                                            (pathname == href
+                                                ? " text-blue-400"
+                                                : " ") +
+                                            (visible
+                                                ? " inline-block py-2 sm:py-0"
+                                                : " hidden sm:inline-block")
+                                        }
+                                        onClick={() => setVisible(false)}
+                                    >
+                                        {label}
+                                    </a>
+                                </Link>
+                            </motion.li>
+                        ))}
+                    </motion.ul>
+                </motion.nav>
             </header>
         </div>
     );
