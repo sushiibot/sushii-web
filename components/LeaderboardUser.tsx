b590b7de66124f17658ff6682deb28516fbc273c
drklee3 <derrick@dlee.dev>
2021-02-18 18:24:59 -0800

    revert coop/coep headers

--- components/LeaderboardUser.tsx
+++ components/LeaderboardUser.tsx
@@ -70,7 +70,6 @@ export default function LeaderboardUser({ node, i, guildId }: any) {
                     style={{ textIndent: "100%" }}
                     src={avatarUrl.replace("?size=1024", "?size=128")}
                     alt={`${name}'s avatar`}
-                    crossOrigin="anonymous"
                 />
                 <span className="text-gray-700 group-hover:text-gray-400">
                     <span className="text-gray-200 group-hover:text-gray-800">
