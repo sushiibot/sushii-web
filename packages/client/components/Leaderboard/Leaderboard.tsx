d2492447c3d4f8573ae4d07bd6bb442e3dd989cb
drklee3 <derrick@dlee.dev>
2021-05-12 12:49:29 -0700

    feat(client): New leaderboard tab design

--- packages/client/components/Leaderboard/Leaderboard.tsx
+++ packages/client/components/Leaderboard/Leaderboard.tsx
@@ -4,6 +4,7 @@ import LeaderboardUserList from "./LeaderboardUserList";
 import NextReset from "./NextReset";
 import { useGraphQLQuery } from "../../lib/useGraphQLQuery";
 import { useLeaderboardQuery, LevelTimeframe } from "@sushii-web/graphql";
+import { motion, AnimateSharedLayout } from "framer-motion";
 
 export interface GuildLeaderboardProps {
     guildId?: string;
@@ -37,23 +38,38 @@ export default function Leaderboard({
                 guildId={guildId}
                 routerIsLoading={routerIsLoading}
             />
-            <div className="flex flex-wrap justify-evenly my-4">
-                {Object.entries(TIMEFRAME_STRS).map(([name, value]) => (
-                    <div key={name}>
-                        <button
-                            className={
-                                "px-4 py-2 my-2 rounded-full hover:bg-gray-800 hover:text-white " +
-                                (value === timeframe
-                                    ? "border border-gray-900 bg-gray-900 text-blue-400"
-                                    : "border border-gray-900 text-gray-400")
-                            }
-                            onClick={() => setTimeframe(value)}
-                        >
-                            {name}
-                        </button>
-                    </div>
-                ))}
-            </div>
+            <AnimateSharedLayout>
+                <motion.ul
+                    layout
+                    className="flex flex-wrap my-6 border-b border-gray-700"
+                >
+                    {Object.entries(TIMEFRAME_STRS).map(([name, value]) => (
+                        <motion.li key={name} className="mr-4 group">
+                            <button
+                                className={
+                                    "focus:outline-none px-4 py-4 border-gray-900  \
+                                    text-gray-400  border-b-2" +
+                                    (value === timeframe
+                                        ? " text-blue-400"
+                                        : " hover:text-white")
+                                }
+                                onClick={() => setTimeframe(value)}
+                            >
+                                {name}
+                            </button>
+                            {value === timeframe ? (
+                                <motion.div
+                                    transition={{ duration: 0.1 }}
+                                    className="w-full h-px bg-blue-400"
+                                    layoutId="underline"
+                                />
+                            ) : (
+                                <div className="w-full h-px group-hover:bg-white" />
+                            )}
+                        </motion.li>
+                    ))}
+                </motion.ul>
+            </AnimateSharedLayout>
             <NextReset timeframe={timeframe} />
             <LeaderboardUserList
                 data={data}
