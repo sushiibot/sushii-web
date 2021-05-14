8275dc5b283314c35f5062872b575ffb86f86594
drklee3 <derrick@dlee.dev>
2021-05-14 15:25:14 -0700

    feat(client): Add table column filtering

--- packages/client/components/Dashboard/RTable.tsx
+++ packages/client/components/Dashboard/RTable.tsx
@@ -1,28 +1,145 @@
 import React, { useMemo } from "react";
-import { useTable, useSortBy, Column } from "react-table";
+import {
+    useTable,
+    useSortBy,
+    Column,
+    useFilters,
+    useGlobalFilter,
+    useAsyncDebounce,
+    Row,
+    FilterProps,
+} from "react-table";
 import {
     SortAscendingIcon,
     SortDescendingIcon,
     SwitchVerticalIcon,
 } from "@heroicons/react/solid";
+import { matchSorter } from "match-sorter";
+
+function GlobalFilter({
+    preGlobalFilteredRows,
+    globalFilter,
+    setGlobalFilter,
+}) {
+    const count = preGlobalFilteredRows.length;
+    const [value, setValue] = React.useState(globalFilter);
+    const onChange = useAsyncDebounce((value) => {
+        setGlobalFilter(value || undefined);
+    }, 200);
+
+    return (
+        <span>
+            Search:{" "}
+            <input
+                className="bg-gray-700 border border-gray-600 focus:border-blue-500
+                           rounded mt-2 p-2 text-white"
+                value={value || ""}
+                onChange={(e) => {
+                    setValue(e.target.value);
+                    onChange(e.target.value);
+                }}
+                placeholder={`${count} tags...`}
+            />
+        </span>
+    );
+}
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+    column: { filterValue, Header, preFilteredRows, setFilter },
+}) {
+    return (
+        <input
+            className="bg-gray-700 border border-gray-600 focus:border-blue-500
+                        rounded mt-2 p-2 text-white"
+            value={filterValue || ""}
+            onChange={(e) => {
+                setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+            }}
+            placeholder={`Search ${Header.toLowerCase()}`}
+        />
+    );
+}
+
+function fuzzyContentObjFilterFn(
+    rows: Row[],
+    id: string[],
+    filterValue: string
+) {
+    return matchSorter(rows, filterValue, {
+        keys: ["original.content"],
+    });
+}
+
+function fuzzyOwnerObjFilterFn(rows: Row[], id: string[], filterValue: string) {
+    return matchSorter(rows, filterValue, {
+        keys: [
+            "original.owner.name",
+            "original.discriminator",
+            "original.ownerId",
+        ],
+    });
+}
+
+function fuzzyTextFilterFn(rows: Row[], id: string[], filterValue: string) {
+    // https://github.com/tannerlinsley/react-table/issues/2644
+    // id is a string[] not string
+    return matchSorter(rows, filterValue, {
+        keys: [(row) => row.values[id[0]]],
+    });
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = (val) => !val;
+fuzzyContentObjFilterFn.autoRemove = (val) => !val;
+fuzzyOwnerObjFilterFn.autoRemove = (val) => !val;
 
 function Table({ columns, data }) {
+    const filterTypes = useMemo(
+        () => ({
+            fuzzyText: fuzzyTextFilterFn,
+            fuzzyOwnerObj: fuzzyOwnerObjFilterFn,
+            fuzzyContentObj: fuzzyContentObjFilterFn,
+        }),
+        []
+    );
+
+    const defaultColumn = useMemo(
+        () => ({
+            // Let's set up our default Filter UI
+            Filter: DefaultColumnFilter,
+        }),
+        []
+    );
+
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
         rows,
         prepareRow,
+        state,
+        preGlobalFilteredRows,
+        setGlobalFilter,
     } = useTable(
         {
             columns,
             data,
+            defaultColumn,
+            filterTypes,
         },
+        useFilters,
+        useGlobalFilter,
         useSortBy
     );
 
     return (
         <>
+            <GlobalFilter
+                preGlobalFilteredRows={preGlobalFilteredRows}
+                globalFilter={state.globalFilter}
+                setGlobalFilter={setGlobalFilter}
+            />
             <table {...getTableProps()}>
                 <thead>
                     {headerGroups.map((headerGroup) => (
@@ -34,11 +151,13 @@ function Table({ columns, data }) {
                                     className="whitespace-nowrap cursor-pointer px-4 py-3
                                     text-left text-sm font-medium tracking-wider 
                                     border-b border-gray-700"
-                                    {...column.getHeaderProps(
-                                        column.getSortByToggleProps()
-                                    )}
                                 >
-                                    <div className="flex">
+                                    <div
+                                        className="flex"
+                                        {...column.getHeaderProps(
+                                            column.getSortByToggleProps()
+                                        )}
+                                    >
                                         {column.render("Header")}
                                         {/* Add a sort direction indicator */}
                                         <span className="ml-2">
@@ -53,6 +172,11 @@ function Table({ columns, data }) {
                                             )}
                                         </span>
                                     </div>
+                                    <div>
+                                        {column.canFilter
+                                            ? column.render("Filter")
+                                            : null}
+                                    </div>
                                 </th>
                             ))}
                         </tr>
@@ -64,7 +188,7 @@ function Table({ columns, data }) {
                         return (
                             <tr
                                 {...row.getRowProps()}
-                                className="even:bg-gray-800"
+                                className="bg-gray-1000 even:bg-gray-800"
                             >
                                 {row.cells.map((cell) => {
                                     return (
--- packages/client/pages/dashboard/[guildId]/tags.tsx
+++ packages/client/pages/dashboard/[guildId]/tags.tsx
@@ -26,102 +26,120 @@ export default function TagsBody() {
             <h2 className="text-lg">
                 {data.tags.totalCount.toLocaleString()} total tags
             </h2>
-            <RTable
-                columns={[
-                    {
-                        accessor: "tagName",
-                        Header: "Tag Name",
-                    },
-                    {
-                        accessor: (row: Tag) => (
-                            <div className="group relative">
-                                <div
-                                    className="w-auto whitespace-nowrap hidden group-hover:inline
+            <div className="bg-gray-800 rounded-2xl p-4 mt-4">
+                <RTable
+                    columns={[
+                        {
+                            accessor: "tagName",
+                            Header: "Tag Name",
+                            filter: "fuzzyText",
+                        },
+                        {
+                            accessor: (row: Tag) => (
+                                <div className="group relative">
+                                    <div
+                                        className="w-auto whitespace-nowrap hidden group-hover:inline
                                         bg-gray-600 rounded p-2
                                         absolute bottom-full"
-                                >
-                                    ID: {row.ownerId}
+                                    >
+                                        ID: {row.ownerId}
+                                    </div>
+                                    {row.owner?.name || `ID ${row.ownerId}`}
                                 </div>
-                                {row.owner?.name || `ID ${row.ownerId}`}
-                            </div>
-                        ),
-                        Header: "Owner",
-                    },
-                    { accessor: "useCount", Header: "Use Count" },
-                    {
-                        accessor: (row: Tag) => (
-                            <ReactMarkdown
-                                className="prose"
-                                remarkPlugins={[gfm]}
-                                linkTarget="_blank"
-                            >
-                                {row.content}
-                            </ReactMarkdown>
-                        ),
-                        Header: "Content",
-                    },
-                ]}
-                data={data.tags.edges.map((t) => t.node)}
-            />
-            <Table
-                columns={[
-                    {
-                        field: "tagName",
-                        header: "Tag Name",
-                        sortable: true,
-                        filter: true,
-                        filterMatchMode: "contains",
-                        filterPlaceholder: "Search name",
-                    },
-                    {
-                        field: "owner.name",
-                        header: "Owner",
-                        sortable: true,
-                        body: (row: Tag) => (
-                            <div className="group relative">
-                                <div
-                                    className="w-auto whitespace-nowrap hidden group-hover:inline
+                            ),
+                            Header: "Owner",
+                            id: "owner",
+                            filter: "fuzzyOwnerObj",
+                        },
+                        {
+                            accessor: "useCount",
+                            Header: "Use Count",
+                        },
+                        {
+                            accessor: (row: Tag) => (
+                                <ReactMarkdown
+                                    className="prose"
+                                    remarkPlugins={[gfm]}
+                                    linkTarget="_blank"
+                                >
+                                    {row.content}
+                                </ReactMarkdown>
+                            ),
+                            Header: "Content",
+                            id: "content",
+                            filter: "fuzzyContentObj",
+                        },
+                    ]}
+                    data={data.tags.edges.map((t) => t.node)}
+                />
+            </div>
+            <div className="hidden">
+                <Table
+                    columns={[
+                        {
+                            field: "tagName",
+                            header: "Tag Name",
+                            sortable: true,
+                            filter: true,
+                            filterMatchMode: "contains",
+                            filterPlaceholder: "Search name",
+                        },
+                        {
+                            field: "owner.name",
+                            header: "Owner",
+                            sortable: true,
+                            body: (row: Tag) => (
+                                <div className="group relative">
+                                    <div
+                                        className="w-auto whitespace-nowrap hidden group-hover:inline
                                         bg-gray-600 rounded p-2
                                         absolute bottom-full"
-                                >
-                                    ID: {row.ownerId}
+                                    >
+                                        ID: {row.ownerId}
+                                    </div>
+                                    {row.owner?.name || `ID ${row.ownerId}`}
                                 </div>
-                                {row.owner?.name || `ID ${row.ownerId}`}
-                            </div>
-                        ),
-                    },
-                    { field: "useCount", header: "Use Count", sortable: true },
-                    {
-                        field: "content",
-                        header: "Content",
-                        sortable: true,
-                        filter: true,
-                        filterMatchMode: "contains",
-                        filterPlaceholder: "Search content",
-                        body: (row) => (
-                            <ReactMarkdown
-                                className="prose"
-                                remarkPlugins={[gfm]}
-                                linkTarget="_blank"
-                            >
-                                {row.content}
-                            </ReactMarkdown>
-                        ),
-                    },
-                    {
-                        header: "",
-                        body: (row) => (
-                            <div className="pr-4 whitespace-nowrap flex items-center">
-                                <button className="text-blue-400">Edit</button>
-                                <button className="text-red-400 ml-4">
-                                    <TrashIcon className="h-6 w-6" />
-                                </button>
-                            </div>
-                        ),
-                    },
-                ]}
-                data={data.tags.edges.map((t) => t.node)}
-            />
+                            ),
+                        },
+                        {
+                            field: "useCount",
+                            header: "Use Count",
+                            sortable: true,
+                        },
+                        {
+                            field: "content",
+                            header: "Content",
+                            sortable: true,
+                            filter: true,
+                            filterMatchMode: "contains",
+                            filterPlaceholder: "Search content",
+                            body: (row) => (
+                                <ReactMarkdown
+                                    className="prose"
+                                    remarkPlugins={[gfm]}
+                                    linkTarget="_blank"
+                                >
+                                    {row.content}
+                                </ReactMarkdown>
+                            ),
+                        },
+                        {
+                            header: "",
+                            body: (row) => (
+                                <div className="pr-4 whitespace-nowrap flex items-center">
+                                    <button className="text-blue-400">
+                                        Edit
+                                    </button>
+                                    <button className="text-red-400 ml-4">
+                                        <TrashIcon className="h-6 w-6" />
+                                    </button>
+                                </div>
+                            ),
+                        },
+                    ]}
+                    data={data.tags.edges.map((t) => t.node)}
+                />
+            </div>
         </Layout>
     );
 }
