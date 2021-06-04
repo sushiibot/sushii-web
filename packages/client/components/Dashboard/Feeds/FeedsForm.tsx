a492bad94a1676514c5b271ee9bf0b4f10f6b316
drklee3 <derrick@dlee.dev>
2021-06-03 17:51:44 -0700

    feat(client): Use separate forms for each feed group

--- packages/client/components/Dashboard/Feeds/FeedsForm.tsx
+++ packages/client/components/Dashboard/Feeds/FeedsForm.tsx
@@ -1,6 +1,13 @@
 import feeds from "./feeds.json";
-import { useForm } from "react-hook-form";
+import {
+    useForm,
+    useFormContext,
+    FormProvider,
+    UseFormRegister,
+    FieldValues,
+} from "react-hook-form";
 import { Feeds, Parameters, ParameterItem, ParameterList } from "./RssBridge";
+import { useEffect, useState } from "react";
 
 const FEEDS: Feeds = feeds;
 const BRIDGES: Feeds["bridges"] = Object.entries(FEEDS.bridges)
@@ -13,19 +20,27 @@ const BRIDGES: Feeds["bridges"] = Object.entries(FEEDS.bridges)
 
 console.log(BRIDGES);
 
+// Rough port of form generation from rss-bridge
+// https://github.com/RSS-Bridge/rss-bridge/blob/d38bc1823293f0c8e2be07be6e7566b4ed650d72/lib/BridgeCard.php#L338-L349
+// https://github.com/RSS-Bridge/rss-bridge/blob/d38bc1823293f0c8e2be07be6e7566b4ed650d72/lib/BridgeCard.php#L62
+
 interface InputProps {
     name: string;
     parameter: ParameterItem;
+    register: UseFormRegister<FieldValues>;
 }
 
-function Input({ name, parameter }: InputProps) {
+function Input({ name, parameter, register }: InputProps) {
     return (
         <div key={name}>
             <label>
                 {parameter.type === "list" ? (
                     <label>
                         <span className="block">{parameter.name}</span>
-                        <select className="bg-gray-700 text-white p-2 my-2">
+                        <select
+                            className="bg-gray-700 text-white p-2 my-2 rounded"
+                            {...register(name)}
+                        >
                             {Object.entries(parameter.values).map(
                                 ([valueDisplay, value]) =>
                                     typeof value === "object" ? (
@@ -58,12 +73,13 @@ function Input({ name, parameter }: InputProps) {
                     <label>
                         <span className="block">{parameter.name}</span>
                         <input
-                            className="bg-gray-700 text-white p-2 my-2"
+                            className="bg-gray-700 text-white p-2 my-2 rounded"
                             type={parameter.type || "text"}
                             defaultValue={(
                                 parameter.defaultValue || ""
                             ).toString()}
                             required={parameter.required}
+                            {...register(name)}
                         />
                     </label>
                 )}
@@ -77,16 +93,55 @@ interface BridgeParametersProps {
     // If this is **not** a group. Group and parameters are both objects with
     // arbitrary keys, so just the 2nd layer is parameters.
     isParameter: boolean;
+    onSubmit: (data: any) => void;
 }
 
-function BridgeParameters({ parameters, isParameter }: BridgeParametersProps) {
-    console.log(parameters, isParameter);
-    console.log(
-        !Array.isArray(parameters) &&
-            typeof parameters === "object" &&
-            !isParameter
-    );
+interface BridgeFormProps {
+    groupName?: string;
+    parameters: ParameterList;
+    onSubmit: (data: any) => void;
+}
 
+function BridgeForm({ groupName, parameters, onSubmit }: BridgeFormProps) {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm({ shouldUnregister: true });
+
+    return (
+        <div className="mt-4 bg-gray-800 p-4 rounded">
+            {groupName && <h4 className="text-xl font-medium">{groupName}</h4>}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="">
+                    {Object.entries(parameters as ParameterList).map(
+                        ([parameterName, parameter]) => (
+                            <Input
+                                key={parameterName}
+                                name={parameterName}
+                                parameter={parameter}
+                                register={register}
+                            />
+                        )
+                    )}
+                    <button
+                        className="bg-blue-500 px-2 py-1 rounded"
+                        type="submit"
+                    >
+                        Add Feed
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+function BridgeParameters({
+    parameters,
+    isParameter,
+    onSubmit,
+}: BridgeParametersProps) {
     // Multiple groups of parameters
     if (
         !Array.isArray(parameters) &&
@@ -96,15 +151,22 @@ function BridgeParameters({ parameters, isParameter }: BridgeParametersProps) {
         return (
             <>
                 {Object.entries(parameters).map(
-                    ([groupName, parameters]: [string, ParameterList]) => (
-                        <div key={groupName} className="mt-4">
-                            <h4 className="text-xl font-medium">{groupName}</h4>
-                            <BridgeParameters
-                                parameters={parameters}
-                                isParameter={true}
+                    ([groupName, groupParameters]: [string, ParameterList]) =>
+                        groupName !== "global" && (
+                            <BridgeForm
+                                key={groupName}
+                                groupName={groupName}
+                                parameters={
+                                    parameters["global"]
+                                        ? {
+                                              ...groupParameters,
+                                              ...parameters["global"],
+                                          }
+                                        : groupParameters
+                                }
+                                onSubmit={onSubmit}
                             />
-                        </div>
-                    )
+                        )
                 )}
             </>
         );
@@ -112,44 +174,29 @@ function BridgeParameters({ parameters, isParameter }: BridgeParametersProps) {
 
     // Default group
     if (Array.isArray(parameters) && parameters.length === 1) {
-        return (
-            <BridgeParameters parameters={parameters[0]} isParameter={true} />
-        );
+        return <BridgeForm parameters={parameters[0]} onSubmit={onSubmit} />;
     } else if (Array.isArray(parameters) && parameters.length === 0) {
         // Empty array, no parameters
         return null;
     }
 
-    // Actual parameters for a single group
-    return (
-        <>
-            {Object.entries(parameters as ParameterList).map(
-                ([parameterName, parameter]) => (
-                    <Input name={parameterName} parameter={parameter} />
-                )
-            )}
-        </>
-    );
+    return null;
 }
 
 export default function FeedsForm() {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm();
+    const [selectedBridgeName, setSelectedBridgeName] = useState<string>(null);
 
-    const watchBridgeName = watch("bridgeName");
-    const watchBridge = watchBridgeName ? BRIDGES[watchBridgeName] : undefined;
+    const selectedBridge = selectedBridgeName
+        ? BRIDGES[selectedBridgeName]
+        : undefined;
 
     const onSubmit = (data) => console.log(data);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <>
             <select
-                {...register("bridgeName")}
-                className="bg-gray-700 text-white"
+                className="bg-gray-700 text-white p-2 my-2 rounded"
+                onChange={(e) => setSelectedBridgeName(e.target.value)}
             >
                 {Object.entries(BRIDGES).map(([bridgeKey, bridge]) => (
                     <option
@@ -162,18 +209,26 @@ export default function FeedsForm() {
                 ))}
             </select>
 
-            {watchBridge && (
+            {selectedBridge && (
                 <div>
-                    <h3>{watchBridge.name}</h3>
-                    <p>{watchBridge.uri}</p>
-                    <p>{watchBridge.description}</p>
+                    <h3>
+                        <span className="text-2xl font-medium">
+                            {selectedBridge.name.replace("Bridge", "")}
+                        </span>
+                        <span className="text-sm text-gray-400">
+                            {" "}
+                            ({selectedBridge.uri})
+                        </span>
+                    </h3>
+                    <p>{selectedBridge.description}</p>
 
                     <BridgeParameters
-                        parameters={watchBridge.parameters}
+                        parameters={selectedBridge.parameters}
                         isParameter={false}
+                        onSubmit={onSubmit}
                     />
                 </div>
             )}
-        </form>
+        </>
     );
 }
